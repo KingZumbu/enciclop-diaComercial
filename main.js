17c25a65dfeabb0ff893713144c8380c3605908f
Igor Ferreira Junqueira Portugal <139009828+KingZumbu@users.noreply.github.com>
2024-04-08 19:46:06 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
-function abrirProcuraNos(){
-    document.getElementById("procureNos_div").classList.remove("hide")
+
+function abrirOculto(aba){
+    document.getElementById(aba).classList.remove("hide")
+}
+
+function gerarFiltro(){
+    const cidadeFiltrada = document.getElementById("filtroLocal").value;
+    
+    const dataLocal = document.querySelectorAll('[data-local]')
+
+    dataLocal.forEach(el => {
+        const cidadeData = el.getAttribute('data-local')
+
+        
+        if(cidadeData === cidadeFiltrada){
+            console.log("aheee")
+        } else {
+            el.classList.add("hide")
+        }
+    })
+    
 }
